ae5d8f26d3cc14f949dff3297f0bb5c8ac92d247
OGNate <nate77w88@gmail.com>
2021-09-26 21:43:59 -0400

    Testing the query of google searches

--- image_scraper.py
+++ image_scraper.py
@@ -2,24 +2,35 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
+"""
+    To-Do:
+        1) Continue reading in images
+"""
+
 DRIVER_PATH = "C:\\Users\\motonite29.DESKTOP-SNHF63L\\Desktop\Scraping\\chromedriver_win32\\chromedriver"
 
 def scraper_main():
     driver = webdriver.Chrome(executable_path = DRIVER_PATH)
-    driver.get("https://www.google.com/")
 
-    google_search(driver)   # Google Searches for an item
-    
+    google_search("trash cans", 10, driver, 1)   # Google Searches for an item
+
 
     time.sleep(2)
 
     driver.quit()   # Exits the driver
 
 
-def google_search(driver):
-    search_bar = driver.find_element_by_name("q")   # Finds the search Bar in google
-    search_bar.send_keys("Hello")
-    search_bar.send_keys(Keys.ENTER)
+def google_search(search_word: str, number_imgs: int, driver: webdriver, sleep_between_intervals: int = 1):
+    
+    def scroll_to_end(driver: webdriver):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(sleep_between_intervals)
+
+    driver.get("https://www.google.com/")
+    search = driver.find_element_by_name("q")
+    search.send_keys(search_word)
+    search.send_keys(Keys.ENTER)
+
 
 
 
