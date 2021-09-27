83f7c8b126055619cdc58a6d2567cb0921f6e8c8
OGNate <nate77w88@gmail.com>
2021-09-26 20:31:12 -0400

    First Commit

---
+++ image_scraper.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+DRIVER_PATH = "C:\\Users\\motonite29.DESKTOP-SNHF63L\\Desktop\Scraping\\chromedriver_win32\\chromedriver"
+
+def scraper_main():
+    driver = webdriver.Chrome(executable_path = DRIVER_PATH)
+    driver.get("https://www.google.com/")
+
+    google_search(driver)   # Google Searches for an item
+
+    time.sleep(2)
+
+    driver.quit()   # Exits the driver
+
+
+def google_search(driver):
+    search_bar = driver.find_element_by_name("q")   # Finds the search Bar in google
+    search_bar.send_keys("Hello")
+    search_bar.send_keys(Keys.ENTER)
+
+
+
+if __name__ == "__main__":
+    scraper_main()
